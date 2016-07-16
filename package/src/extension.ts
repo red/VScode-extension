1087fee8cc6f91ef26127de69dda59812a668f9a
red-eco <eco@red-lang.org>
2016-07-16 17:47:37 +0800

    stdout_read_encoding 'utf16le'

--- package/src/extension.ts
+++ package/src/extension.ts
@@ -13,6 +13,7 @@ var red_menu = [];
 var _exe = '';
 var red_bin_path = '';
 var red_bin_exist = false;
+var stdout_read_encoding = {encoding:'utf8'};
 
 function mkdirpSync(dirpath) {
     var parts = dirpath.split(path.sep);
@@ -94,8 +95,8 @@ function handle_command(command) {
                 red_bin_exist = true;
                 vscode.window.showInformationMessage('Please wait, compile gui-console...');
                 var str = red_bin_path + ' ' + red_extensionPath + '/bin/' + process.platform + '/keep-me.red';
-                //child_process.exec(str, null, function (error, stdout, stderr) {
-                child_process.execFile(red_bin_path, [red_extensionPath + '/bin/' + process.platform + '/keep-me.red'], null, function (error, stdout, stderr) {
+                //child_process.exec(str, stdout_read_encoding, function (error, stdout, stderr) {
+                child_process.execFile(red_bin_path, [red_extensionPath + '/bin/' + process.platform + '/keep-me.red'], stdout_read_encoding, function (error, stdout, stderr) {
                     var output;
                     if (error != null) {
                         output = error.message;
@@ -173,7 +174,7 @@ export function activate(context: vscode.ExtensionContext) {
     
     if ('win32' == process.platform) {
         _exe = '.exe';
-        child_process.stdout.setEncoding('ucs2');
+        stdout_read_encoding.encoding = 'utf16le';
     }
     red_bin_path = red_extensionPath + '/bin/' + process.platform + '/';
     //fs.existsSync(red_bin_path) || fs.mkdirSync(red_bin_path);
